b3c3cb19a34a17cf0fe6e7e763837f3b1343acac
yyz1124 <amy87326@163.com>
2018-03-18 18:40:15 +0800

    implement group CRUD

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,57 @@
 class GroupsController < ApplicationController
 
+before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+
+
    def index
       @groups = Group.all
-      flash[:notice] = "登录成功"
    end
+
+   def show
+      @group = Group.find(params[:id])
+   end
+
+   def new
+      @group = Group.new
+   end
+
+   def create
+      @group = Group.new(group_path)
+
+      if @group.save
+         redirect_to group_path
+      else
+         render :new
+      end
+   end
+
+
+
+   def edit
+      @group = Group.find(params[:id])
+   end
+
+   def update
+      @group = Group.find(params[:id])
+      if @group.update(group_params)
+         redirect_to groups_path
+      else
+         render :edit
+      end
+   end
+
+
+   def destroy
+      @group = Group.find(params[:id])
+      @group.destroy
+      redirect_to groups_path
+
+   end
+
+   private
+
+   def group_params
+      params.require(:group).permit(:title, :description)
+   end
+
 end
