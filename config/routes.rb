7d023481f93226e580fb5ae7390a1c3379ec9187
yyz1124 <amy87326@163.com>
2018-03-21 22:50:41 +0800

    admin views

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :groups
-  root 'groups#index'
+  root 'welcome#index'
 
 
   namespace :admin do
-     resources :groups 
+     resources :groups
   end
 
 end
